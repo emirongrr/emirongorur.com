7d7cd52a2552a6d0379363ad8d2bf1b76324d28d
emirongrr <emirongrr@gmail.com>
2024-12-25 20:43:53 +0300

    fix:hydration errors

--- src/app/components/Navbar/theme.tsx
+++ src/app/components/Navbar/theme.tsx
@@ -2,9 +2,18 @@
 import { useTheme } from "next-themes";
 import SunIcon from "../../assets/icons/sun";
 import MoonIcon from "../../assets/icons/moon";
+import { useEffect, useState } from "react";
 
 export default function Theme() {
   const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   const currentTheme = theme === "system" ? systemTheme : theme;
 
   function toggleTheme() {
@@ -14,7 +23,7 @@ export default function Theme() {
   return (
     <button
       onClick={toggleTheme}
-      className={` dark:text-primary-color text-zinc-500 border dark:border-zinc-800 border-zinc-200 rounded-full p-2 duration-300 transition-transform group: ${
+      className={`dark:text-primary-color text-zinc-500 border dark:border-zinc-800 border-zinc-200 rounded-full p-2 duration-300 transition-transform group: ${
         currentTheme === "light" ? "-rotate-180" : "rotate-0"
       }`}
       aria-label="Toggle Theme"
